7a579eae9c874a861056068eb64510eb12510529
saurav ag. <133648739+Srv2k21Codex@users.noreply.github.com>
2024-09-20 13:48:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,274 @@
+// script.js
+
+let questions = [
+    {
+        prompt: `How cna a variable be declared in
+                 javascript ?`,
+        options: [
+            "var strname",
+            "strname var",
+            "val str",
+            "str",
+        ],
+        answer: "var strname",
+    },
+
+    {
+        prompt: `How do you call a
+                 function named 
+                 myFunction?`,
+        options: [
+            "call myFunction()",
+            "myFunction()",
+            "call function myFunction",
+            "Call.myFunction",
+        ],
+        answer: "myFunction()",
+    },
+
+    {
+        prompt: `which is NOT a comparison
+                  operator in javascript`,
+        options: [
+            "==",
+            ">=",
+            "++x",
+            " <",
+        ],
+        answer: "++x",
+    },
+
+    {
+        prompt: `The stylesheet file will not be
+                  loaded by the browser if you omit`,
+        options: ["REL", "STYLE", "BODY", "HTML"],
+        answer: "REL",
+    },
+
+    {
+        prompt: `what are the three important 
+                  manipulations for a loop on a loop
+                  variable.`,
+        options: [
+            "updation,incrementation,initialization",
+            "initialization,testing,incrementation",
+            "Testing ,updation,Testing",
+            "Initialization,Testing,Updation",
+        ],
+        answer: "Initializatoin,Testing,Updation",
+    },
+];
+
+// Get Dom Elements
+
+let questionsEl =
+    document.querySelector(
+        "#questions"
+    );
+let timerEl =
+    document.querySelector("#timer");
+let choicesEl =
+    document.querySelector("#options");
+let submitBtn = document.querySelector(
+    "#submit-score"
+);
+let startBtn =
+    document.querySelector("#start");
+let nameEl =
+    document.querySelector("#name");
+let feedbackEl = document.querySelector(
+    "#feedback"
+);
+let reStartBtn =
+    document.querySelector("#restart");
+
+// Quiz's initial state
+let currentQuestionIndex = 0;
+let time = questions.length * 15;
+let timerId;
+
+// Start quiz and hide frontpage
+
+function quizStart() {
+    timerId = setInterval(
+        clockTick,
+        1000
+    );
+    timerEl.textContent = time;
+    let landingScreenEl =
+        document.getElementById(
+            "start-screen"
+        );
+    landingScreenEl.setAttribute(
+        "class",
+        "hide"
+    );
+    questionsEl.removeAttribute(
+        "class"
+    );
+    getQuestion();
+}
+
+// Loop through array of questions and
+// Answers and create list with buttons
+function getQuestion() {
+    let currentQuestion =
+        questions[currentQuestionIndex];
+    let promptEl =
+        document.getElementById(
+            "question-words"
+        );
+    promptEl.textContent =
+        currentQuestion.prompt;
+    choicesEl.innerHTML = "";
+    currentQuestion.options.forEach(
+        function (choice, i) {
+            let choiceBtn =
+                document.createElement(
+                    "button"
+                );
+            choiceBtn.setAttribute(
+                "value",
+                choice
+            );
+            choiceBtn.textContent =
+                i + 1 + ". " + choice;
+            choiceBtn.onclick =
+                questionClick;
+            choicesEl.appendChild(
+                choiceBtn
+            );
+        }
+    );
+}
+
+// Check for right answers and deduct
+// Time for wrong answer, go to next question
+
+function questionClick() {
+    if (
+        this.value !==
+        questions[currentQuestionIndex]
+            .answer
+    ) {
+        time -= 10;
+        if (time < 0) {
+            time = 0;
+        }
+        timerEl.textContent = time;
+        feedbackEl.textContent = `Wrong! The correct answer was 
+        ${questions[currentQuestionIndex].answer}.`;
+        feedbackEl.style.color = "red";
+    } else {
+        feedbackEl.textContent =
+            "Correct!";
+        feedbackEl.style.color =
+            "green";
+    }
+    feedbackEl.setAttribute(
+        "class",
+        "feedback"
+    );
+    setTimeout(function () {
+        feedbackEl.setAttribute(
+            "class",
+            "feedback hide"
+        );
+    }, 2000);
+    currentQuestionIndex++;
+    if (
+        currentQuestionIndex ===
+        questions.length
+    ) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
+}
+
+// End quiz by hiding questions,
+// Stop timer and show final score
+
+function quizEnd() {
+    clearInterval(timerId);
+    let endScreenEl =
+        document.getElementById(
+            "quiz-end"
+        );
+    endScreenEl.removeAttribute(
+        "class"
+    );
+    let finalScoreEl =
+        document.getElementById(
+            "score-final"
+        );
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute(
+        "class",
+        "hide"
+    );
+}
+
+// End quiz if timer reaches 0
+
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+// Save score in local storage
+// Along with users' name
+
+function saveHighscore() {
+    let name = nameEl.value.trim();
+    if (name !== "") {
+        let highscores =
+            JSON.parse(
+                window.localStorage.getItem(
+                    "highscores"
+                )
+            ) || [];
+        let newScore = {
+            score: time,
+            name: name,
+        };
+        highscores.push(newScore);
+        window.localStorage.setItem(
+            "highscores",
+            JSON.stringify(highscores)
+        );
+        alert(
+            "Your Score has been Submitted"
+        );
+    }
+}
+
+// Save users' score after pressing enter
+
+function checkForEnter(event) {
+    if (event.key === "Enter") {
+        saveHighscore();
+        alert(
+            "Your Score has been Submitted"
+        );
+    }
+}
+
+document.getElementById("toggle-mode").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+});
+
+
+
+nameEl.onkeyup = checkForEnter;
+
+// Save users' score after clicking submit
+
+submitBtn.onclick = saveHighscore;
+
+// Start quiz after clicking start quiz
+
+startBtn.onclick = quizStart;
